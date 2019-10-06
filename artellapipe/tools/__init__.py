b2e3a7c28d8f833f10b6f8ac1405b9285b19af73
Tomi Poveda <tpovedatd@gmail.com>
2019-10-06 13:48:43 +0200

    Fixed setup.cfg file

--- artellapipe/tools/__init__.py
+++ artellapipe/tools/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """
-Initialization module for artellapipe
+Initialization module for artellapipe-tools
 """
 
 from __future__ import print_function, division, absolute_import
