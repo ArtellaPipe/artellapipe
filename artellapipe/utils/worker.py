041b8f6b03e2ba8451d6ca4e4af42f0ac98e1961
Tomi Poveda <tpovedatd@gmail.com>
2019-11-10 03:01:51 +0100

    Added worker to update thumbnails using threads

--- artellapipe/utils/worker.py
+++ artellapipe/utils/worker.py
@@ -12,13 +12,20 @@ __license__ = "MIT"
 __maintainer__ = "Tomas Poveda"
 __email__ = "tpovedatd@gmail.com"
 
+import os
 import uuid
+import logging
 import traceback
 from threading import Lock, Condition
 
 from Qt.QtCore import *
+from Qt.QtGui import *
 
 from tpQtLib.core import qtutils
+
+import artellapipe
+
+LOGGER = logging.getLogger()
 
 
 class Worker(QThread, object):
@@ -198,3 +205,55 @@ class QtWorker(QThread, object):
                     if self._execute_tasks:
                         data = data if data is not None else dict()
                         self.workCompleted.emit(item_to_process['id'], data)
+
+
+class ThumbDownloaderWorker(QRunnable, object):
+
+    class ThumbDownloaderWorkerSignals(QObject, object):
+        triggered = Signal(object)
+
+    def __init__(self, *args):
+        super(ThumbDownloaderWorker, self).__init__(*args)
+
+        self._path = None
+        self._preview_id = None
+        self._force = False
+        self.signals = ThumbDownloaderWorker.ThumbDownloaderWorkerSignals()
+
+    def set_preview_id(self, preview_id):
+        """
+        Sets the preview ID linked to Kitsu project
+        :param preview_id: str
+        """
+
+        self._preview_id = preview_id
+
+    def set_path(self, path):
+        """
+        Sets the path where image should be downloaded
+        :param path: str
+        """
+
+        self._path = path
+
+    def set_force(self, flag):
+        """
+        Sets whether thumb should be downloaded even if it exists
+        :param flag: bool
+        """
+
+        self._force = flag
+
+    def run(self):
+        try:
+            if self._preview_id and self._path:
+                if not os.path.isfile(self._path) or self._force:
+                    artellapipe.Tracker().download_preview_file_thumbnail(self._preview_id, self._path)
+                if not self._path or not os.path.isfile(self._path):
+                    icon = None
+                else:
+                    icon_pixmap = QPixmap(self._path)
+                    icon = QIcon(icon_pixmap)
+                self.signals.triggered.emit(icon)
+        except Exception as exc:
+            LOGGER.error('Cannot load thumbnail image: {}!'.format(exc))
