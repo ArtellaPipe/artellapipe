3400f6a55f124f639143fe01c559059eaba23b22
Tomas Poveda <tpovedatd@gmail.com>
2021-01-29 23:52:23 +0100

    Added module that contains constant definitions for artellapipe

---
+++ artellapipe/core/consts.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Module that contains constanta definitions used by artellapipe-core
+"""
+
+from __future__ import print_function, division, absolute_import
+
+PACKAGE = 'artellapipe'
